dfbf3635e8bb9ef1977aad1a8f2ffa5d8c9a418a
Thibault Houdon <houdon.thibault@gmail.com>
2024-04-28 16:02:28 +0100

    Refactor main python file.

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-import arrow
-from flask import Flask, render_template, request, jsonify
+import os
+from flask import Flask, render_template, request, jsonify, make_response
 import requests
 from urllib.parse import urlparse, parse_qs
-from dotenv import load_dotenv
-import os
 
-from flask import make_response
+from flask import Response
 from isodate import parse_duration
+from datetime import timedelta
+import arrow
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -14,114 +16,105 @@ YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
 app = Flask(__name__)
 
 
+def get_video_id_from_request() -> str:
+    """Extract the video ID from the request parameters or use a default."""
+    if video_url := request.args.get('video_url'):
+        return extract_video_id(video_url)
+    return "LamjAFnybo0"
+
+
 @app.route('/', methods=['GET'])
-def index():
-    url_base = "https://www.youtube.com/watch?v=LamjAFnybo0"
-    video_id = extract_video_id(url_base)
-    infos = get_video_infos(video_id=video_id)
+def index() -> str:
+    """Render the main page."""
+    video_id = get_video_id_from_request()
+    language = request.args.get('language', "en").lower()
+    infos = fetch_video_info(video_id, language)
 
     return render_template('index.html', **infos)
 
 
 @app.route('/get-video-info', methods=['POST'])
-def get_video_info():  # HTMX
+def get_video_info() -> Response | str:
+    """Endpoint to get video information from YouTube API."""
     video_url = request.form.get('video_url')
+    language = request.form.get('language')
     if not video_url:
-        return send_error("Please provide a YouTube URL.")
+        return "No video URL provided."
 
     video_id = extract_video_id(video_url)
     if not video_id:
-        return send_error("Invalid YouTube URL provided.")
+        return "Invalid video URL."
 
-    infos = get_video_infos(video_id=video_id)
+    infos = fetch_video_info(video_id, language)
     if infos:
         return render_template('components/card.html', **infos)
 
-    return send_error("No data found for this URL.")
+    # TODO: Handle error when url is not a valid YouTube video URL
+    return "Error fetching video information."
 
 
-def send_error(message):
-    """ Helper function to send error messages with HTMX. """
-    resp = make_response(message)
-    resp.headers['HX-Retarget'] = '#form-error'
-    resp.headers['HX-Trigger'] = 'errorTrigger'
-    return resp
-
-
-def get_video_infos(video_id) -> dict:
+def fetch_video_info(video_id: str, language: str) -> dict:
+    """Fetch video details from YouTube API."""
     youtube_api_url = f'https://www.googleapis.com/youtube/v3/videos?id={video_id}&key={YOUTUBE_API_KEY}&part=snippet,contentDetails,statistics'
     response = requests.get(youtube_api_url)
-    infos = {}
+    if response.status_code != 200:
+        return {}
 
-    if response.status_code == 200:
-        data = response.json()
-        items = data.get('items')
-        if not items:
-            return {}
-        video_info = items[0]
-        snippet = video_info['snippet']
-        content_details = video_info['contentDetails']
-        statistics = video_info['statistics']
-        channel_id = snippet.get('channelId')
-
-        thumbnails = snippet.get('thumbnails', {})
-        published_at = snippet.get('publishedAt', {})
-        maxres_thumbnail = thumbnails.get('maxres', {}).get('url')
-        high_thumbnail = thumbnails.get('high', {}).get('url')
-        default_thumbnail = thumbnails.get('default', {}).get('url')
-
-        duration_iso8601 = content_details.get('duration')
-        video_duration = format_duration(duration_iso8601)
-
-        infos["video_thumbnail_url"] = maxres_thumbnail or high_thumbnail or default_thumbnail
-        infos["title"] = snippet.get('title')
-        infos["channel_title"] = snippet.get('channelTitle')
-        infos["view_count"] = statistics.get('viewCount')
-        infos["channel_thumbnail_url"] = get_channel_thumbnail_url(channel_id)
-        infos["video_duration"] = video_duration
-        infos["published_at"] = arrow.get(published_at).humanize()
-
-    return infos
+    if data := response.json().get('items', []):
+        return parse_video_info(data[0], language)
 
 
-def get_channel_thumbnail_url(channel_id) -> str:
+def parse_video_info(video_info: dict, language: str) -> dict:
+    """Parse video information from the API response."""
+    snippet = video_info['snippet']
+    content_details = video_info['contentDetails']
+    statistics = video_info['statistics']
+
+    return {
+        "video_thumbnail_url": select_thumbnail(snippet.get('thumbnails', {})),
+        "title": snippet.get('title', "Title not found"),
+        "channel_title": snippet.get('channelTitle', "Channel not found"),
+        "view_count": statistics.get('viewCount', "0"),
+        "channel_thumbnail_url": get_channel_thumbnail_url(snippet.get('channelId')),
+        "video_duration": format_duration(content_details.get('duration')),
+        "published_at": arrow.get(snippet.get('publishedAt')).humanize(locale=language)
+    }
+
+
+def select_thumbnail(thumbnails: dict) -> str:
+    """Select the best available thumbnail."""
+    return thumbnails.get('maxres', {}).get('url') or \
+        thumbnails.get('high', {}).get('url') or \
+        thumbnails.get('default', {}).get('url')
+
+
+def get_channel_thumbnail_url(channel_id: str) -> str:
+    """Fetch channel thumbnail from YouTube API."""
     channel_data_url = f'https://www.googleapis.com/youtube/v3/channels?part=snippet&id={channel_id}&key={YOUTUBE_API_KEY}'
     response = requests.get(channel_data_url)
-    if response.status_code == 200:
-        data = response.json()
-        items = data.get('items')
-        if not items:
-            return None
-        channel_info = items[0]
-        snippet = channel_info['snippet']
-        thumbnails = snippet.get('thumbnails', {})
-        high_thumbnail = thumbnails.get('high', {}).get('url')
-        default_thumbnail = thumbnails.get('default', {}).get('url')
-        return high_thumbnail if high_thumbnail else default_thumbnail
+    if response.status_code != 200:
+        return ""
+
+    channel_info = response.json().get('items', [{}])[0]
+    thumbnails = channel_info.get('snippet', {}).get('thumbnails', {})
+    return thumbnails.get('high', {}).get('url') or thumbnails.get('default', {}).get('url')
 
 
-def format_duration(duration_iso8601):
-    # Parse la durée ISO 8601 en un objet timedelta
-    video_duration_timedelta = parse_duration(duration_iso8601)
-
-    # Calcul des heures, minutes et secondes
-    hours, remainder = divmod(video_duration_timedelta.total_seconds(), 3600)
+def format_duration(duration_iso8601: str) -> str:
+    """Convert ISO8601 duration to a human-readable format."""
+    duration = parse_duration(duration_iso8601)
+    hours, remainder = divmod(duration.total_seconds(), 3600)
     minutes, seconds = divmod(remainder, 60)
-
-    # Formate la chaîne en fonction de la présence ou non des heures
-    if hours > 0:
+    if hours:
         return f'{int(hours)}:{int(minutes):02}:{int(seconds):02}'
-    else:
-        return f'{int(minutes)}:{int(seconds):02}'
+    return f'{int(minutes)}:{int(seconds):02}'
 
 
-def extract_video_id(url):
-    # Extrait l'ID de la vidéo à partir de l'URL
+def extract_video_id(url: str) -> str:
+    """Extract YouTube video ID from URL."""
     parsed_url = urlparse(url)
     video_id = parse_qs(parsed_url.query).get('v')
-    if video_id:
-        return video_id[0]
-    return None
+    return video_id[0] if video_id else None
 
 
 if __name__ == '__main__':
